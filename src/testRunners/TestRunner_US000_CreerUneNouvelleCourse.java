f4902cf2fa968ac86fe173e8989d9d93c3591f41
MFlash29 <mika.sam.std@gmail.com>
2019-01-08 16:02:10 +0100

    Connection avec GitHub

--- src/testRunners/TestRunner_US000_CreerUneNouvelleCourse.java
+++ src/testRunners/TestRunner_US000_CreerUneNouvelleCourse.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="resources/features", glue="")
+@CucumberOptions(features="resources/features", glue="stepDefinitions")
 
 public class TestRunner_US000_CreerUneNouvelleCourse {
 
